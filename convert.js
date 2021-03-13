c5cf548d6a35f76d66edcbbc7dd875ff21a6216e
Gerald Pape <mail@geraldpape.io>
2021-03-13 11:13:42 +0100

    switch to data extraction from official map

---
+++ convert.js
@@ -0,0 +1,104 @@
+import got from "got";
+import { writeFileSync } from "fs";
+import sanitizeHtml from "sanitize-html";
+
+const url = "https://geo.stadt-muenster.de/coronatests/data.php";
+
+const loadData = async () => {
+  const response = await got.get(url);
+  // endpoint is a jsonp endpoint, strip away stuff that makes JSON.parse fail
+  let jsonStr = response.body;
+  jsonStr = jsonStr.slice(0, jsonStr.lastIndexOf(";"));
+  jsonStr = jsonStr.replace(/var.*=/i, "");
+  return JSON.parse(jsonStr);
+};
+
+// copied from original map https://geo.stadt-muenster.de/coronatests/
+const anmeldType = {
+  A_TELMAIL: "Anmeldung per Mail oder telefonisch",
+  A_HOMEPAGE: "Anmeldung online",
+  X: "Anmeldung nicht erforderlich",
+};
+
+const artType = {
+  ARZT: "Arzt",
+  APO: "Apotheke",
+  T_ZENTER: "Testcenter",
+};
+
+const execute = async () => {
+  const json = await loadData();
+
+  const fc = { type: "FeatureCollection", features: [] };
+  const data = [];
+
+  // sanitize properties
+  for (const feature of json.features) {
+    //const { name, tel1, email, str_name, hsnr, anmeld, besonders, homepage } = feature.properties;
+    let { name, str_name, hsnr, ...props } = feature.properties;
+
+    const address = `${sanitizeHtml(str_name)} ${sanitizeHtml(hsnr)}`.trim();
+
+    name = sanitizeHtml(name).trim();
+
+    const info = Object.entries(props)
+      .sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
+      .map(([key, value]) => {
+        if (key === "art") {
+          return artType[value];
+        }
+        if (key === "anmeld") {
+          return anmeldType[value];
+        }
+
+        const safeValue = sanitizeHtml(value).trim();
+
+        if (safeValue && key === "email") {
+          return `<a href="mailto:${safeValue}">${safeValue}</a>`;
+        }
+        if (safeValue && key === "homepage") {
+          return `<a target="_blank" rel="noopener" href="${safeValue}">${safeValue}</a>`;
+        }
+        if (safeValue && key === "tel1") {
+          return `<a href="tel:${safeValue}">${safeValue}</a>`;
+        }
+        if (safeValue && key === "besonders") {
+          return `Hinweis: ${safeValue}`;
+        }
+
+        return safeValue;
+      })
+      .filter((p) => p)
+      .join(", ");
+
+    const properties = {
+      name,
+      address,
+      info,
+    };
+
+    fc.features.push({
+      type: "Feature",
+      properties,
+      geometry: feature.geometry,
+    });
+
+    data.push(properties);
+  }
+
+  // sort by name
+  fc.features.sort(
+    ({ properties: { name: nameA } }, { properties: { name: nameB } }) =>
+      nameA.localeCompare(nameB)
+  );
+
+  data.sort(({name: nameA}, {name: nameB}) => nameA.localeCompare(nameB));
+
+  writeFileSync(
+    "./corona-testungen-muenster-extracted.json",
+    JSON.stringify(fc, undefined, 2)
+  );
+  writeFileSync("./data.json", JSON.stringify(data, undefined, 2));
+};
+
+execute();
